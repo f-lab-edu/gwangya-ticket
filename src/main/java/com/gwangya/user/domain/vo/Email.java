a9cbc2ca52416d865e572eb67edf6e41d01b16a1
SOHEELEE408 <dlwlsrl33@gmail.com>
2023-11-11 21:41:51 +0900

    feat : equals(), hashCode() 오버라이딩

--- src/main/java/com/gwangya/user/domain/vo/Email.java
+++ src/main/java/com/gwangya/user/domain/vo/Email.java
@@ -49,4 +49,17 @@ public class Email {
     public String getValue() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Email email = (Email) o;
+        return Objects.equals(getValue(), email.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
+    }
 }
--- src/main/java/com/gwangya/user/domain/vo/Password.java
+++ src/main/java/com/gwangya/user/domain/vo/Password.java
@@ -42,4 +42,17 @@ public class Password {
     public String getValue() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Password password = (Password) o;
+        return Objects.equals(getValue(), password.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
+    }
 }
