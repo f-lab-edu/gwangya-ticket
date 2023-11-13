fd534c31cfaf616ca5aa807d89a79e716e3de80a
SOHEELEE408 <dlwlsrl33@gmail.com>
2023-11-14 00:14:50 +0900

    feat : Security 설정 추가

--- src/main/java/com/gwangya/global/config/SecurityConfig.java
+++ src/main/java/com/gwangya/global/config/SecurityConfig.java
@@ -2,16 +2,57 @@ package com.gwangya.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.gwangya.global.filter.JwtAuthFilter;
+import com.gwangya.user.service.UserService;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder encoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+	private final JwtAuthFilter jwtAuthFilter;
+
+	private final UserService userService;
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+			.csrf(CsrfConfigurer::disable)
+			.authorizeHttpRequests(request ->
+				request
+					.requestMatchers("/api/auth/**").permitAll()
+					.anyRequest().authenticated()
+			).sessionManagement(session ->
+				session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			).formLogin(FormLoginConfigurer::disable)
+			.authenticationProvider(authenticationProvider())
+			.addFilter(jwtAuthFilter)
+			.build();
+	}
+
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider(encoder());
+		authenticationProvider.setUserDetailsService(userService);
+		return authenticationProvider;
+	}
 }
+
