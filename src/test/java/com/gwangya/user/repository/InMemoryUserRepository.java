b01e974056c31045d6b72ebeae415fe2847e8852
SOHEELEE408 <dlwlsrl33@gmail.com>
2023-11-19 22:07:05 +0900

    feat : 테스트 용 repository 메서드 구현 추가

--- src/test/java/com/gwangya/user/repository/InMemoryUserRepository.java
+++ src/test/java/com/gwangya/user/repository/InMemoryUserRepository.java
@@ -2,6 +2,7 @@ package com.gwangya.user.repository;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import com.gwangya.user.domain.User;
 
@@ -10,16 +11,25 @@ public class InMemoryUserRepository implements UserRepository {
 	private final Map<Long, User> users = new HashMap<>();
 
 	@Override
-	public User save(User user) {
+	public User save(final User user) {
 		Long id = Long.valueOf(users.size() + 1);
 		users.put(id, user);
 		return user;
 	}
 
 	@Override
-	public boolean existsUserByEmail(String email) {
+	public boolean existsUserByEmail(final String email) {
 		return users.values()
 			.stream()
 			.anyMatch(user -> user.getEmail().equals(email));
 	}
+
+	@Override
+	public Optional<User> findByEmail(final String email) {
+		return users.values()
+			.stream()
+			.filter(user -> user.getEmail().equals(email))
+			.findFirst();
+	}
+
 }
