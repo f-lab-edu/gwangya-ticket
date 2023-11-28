888e4deec1edd75c183d85513252d0bb6bcfc53c
SOHEELEE408 <dlwlsrl33@gmail.com>
2023-11-29 01:22:08 +0900

    refactor : password encoder 구현체 변경에 따른 테스트 코드 수정

--- src/test/java/com/gwangya/global/config/InMemoryPasswordEncoder.java
+++ src/test/java/com/gwangya/global/config/InMemoryPasswordEncoder.java
@@ -3,16 +3,16 @@ package com.gwangya.global.config;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class InMemoryPasswordEncoder implements PasswordEncoder {
-    @Override
-    public String encode(CharSequence rawPassword) {
-        return "{bcrypt} " + rawPassword;
-    }
+	@Override
+	public String encode(CharSequence rawPassword) {
+		return "$2a$" + rawPassword;
+	}
 
-    @Override
-    public boolean matches(CharSequence rawPassword, String encodedPassword) {
-        if (encodedPassword.contains(rawPassword)) {
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean matches(CharSequence rawPassword, String encodedPassword) {
+		if (encodedPassword.contains(rawPassword)) {
+			return true;
+		}
+		return false;
+	}
 }
--- src/test/java/com/gwangya/global/config/SecurityConfigTest.java
+++ src/test/java/com/gwangya/global/config/SecurityConfigTest.java
@@ -6,13 +6,19 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootTest(classes = SecurityConfig.class)
+import com.gwangya.user.service.AuthService;
+
+@SpringBootTest(classes = {SecurityConfig.class, AuthService.class})
 class SecurityConfigTest {
 
 	@Autowired
 	PasswordEncoder passwordEncoder;
+
+	@MockBean
+	AuthService authService;
 
 	@DisplayName("비밀번호가 정상적으로 암호화 된다.")
 	@Test
@@ -24,7 +30,7 @@ class SecurityConfigTest {
 		String result = passwordEncoder.encode(password);
 
 		// then
-		assertThat(result).contains("bcrypt");
+		assertThat(result).contains("$2a$");
 		assertThat(passwordEncoder.matches("sohee408!", result)).isTrue();
 	}
 }
