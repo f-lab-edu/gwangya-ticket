1904018912f0e702058d34a208bb3465710cd813
SOHEELEE408 <dlwlsrl33@gmail.com>
2024-02-13 11:57:05 +0900

    refactor : 생성자 직접 사용대신 픽스처로 변경

--- src/test/java/com/gwangya/user/domain/UserFixture.java
+++ src/test/java/com/gwangya/user/domain/UserFixture.java
@@ -11,4 +11,9 @@ public class UserFixture {
 		return new User(id, new Email("test@email.com"), new Password("password"), LocalDateTime.now(),
 			LocalDateTime.now());
 	}
+
+	public static User createUser(Long id, String email, String password) {
+		return new User(id, new Email(email), new Password(password), LocalDateTime.now(),
+			LocalDateTime.now());
+	}
 }
