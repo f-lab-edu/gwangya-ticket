6eeccddc6b7b59edf2a4acf01c86fe14363fbe0d
SOHEELEE408 <dlwlsrl33@gmail.com>
2024-02-18 07:20:08 +0900

    feat : lock 해제 이벤트 추가(시간 초과로 삭제 되었을 때)

--- src/main/java/com/gwangya/lock/LockReleaseEventListener.java
+++ src/main/java/com/gwangya/lock/LockReleaseEventListener.java
@@ -5,20 +5,29 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import com.hazelcast.core.EntryEvent;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.cp.lock.FencedLock;
+import com.hazelcast.map.listener.EntryExpiredListener;
 import com.hazelcast.map.listener.EntryRemovedListener;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-public class LockReleaseEventListener implements EntryRemovedListener<Long, Long> {
+public class LockReleaseEventListener implements EntryRemovedListener<Long, Long>, EntryExpiredListener<Long, Long> {
 
 	@Qualifier("hazelcastInstance")
 	private final HazelcastInstance hazelcastInstance;
 
 	@Override
 	public void entryRemoved(EntryEvent<Long, Long> event) {
-		Long key = event.getKey();
-		FencedLock lock = hazelcastInstance.getCPSubsystem().getLock(String.valueOf(key));
+		lockRelease(event.getKey());
+	}
+
+	@Override
+	public void entryExpired(EntryEvent<Long, Long> event) {
+		lockRelease(event.getKey());
+	}
+
+	private void lockRelease(final Long key) {
+		final FencedLock lock = hazelcastInstance.getCPSubsystem().getLock(String.valueOf(key));
 		lock.destroy();
 	}
 }
