886630053e5bdd6a8c28d961f4c1d04aa4ef7453
SOHEELEE408 <dlwlsrl33@gmail.com>
2024-02-18 20:04:14 +0900

    docs : 주석 추가

--- src/main/java/com/gwangya/purchase/service/PurchaseService.java
+++ src/main/java/com/gwangya/purchase/service/PurchaseService.java
@@ -12,8 +12,10 @@ import com.gwangya.global.exception.EntityNotFoundException;
 import com.gwangya.performance.domain.Seat;
 import com.gwangya.performance.exception.UnavailablePurchaseException;
 import com.gwangya.performance.repository.SeatRepository;
+import com.gwangya.purchase.domain.PurchaseSeat;
 import com.gwangya.purchase.dto.OccupySeatInfo;
 import com.gwangya.purchase.repository.PurchaseSeatRepository;
+import com.hazelcast.map.IMap;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,6 +27,13 @@ public class PurchaseService {
 
 	private final PurchaseSeatRepository purchaseSeatRepository;
 
+	/**
+	 * DB에 반영 전인 {@link PurchaseSeat}도 확인하기 위해 트랜잭션 격리수준을 READ_UNCOMMITTED로 설정합니다.
+	 * 이 메서드는 '좌석 선택' 단계에서 호출됩니다. 좌석 선택과 예매(PurchaseSeat 저장)는 별개의 트랜잭션으로 이루어지고,
+	 * 좌석 선택 데이터는 DB가 아닌 별도의 {@link IMap}에 저장됩니다.
+	 * 따라서 commit 전의 PurchaseSeat은 다른 요청에 의해 선점된 {@link Seat}일 수 있습니다.
+	 * READ_UNCOMMITTED은 예매 중인 Seat을 유효한 것으로 간주하는 것을 방지합니다.
+	 */
 	@Transactional(readOnly = true, isolation = Isolation.READ_UNCOMMITTED)
 	public void checkValidSeat(final OccupySeatInfo occupySeatInfo) {
 		final List<Seat> seats = seatRepository.findAllById(occupySeatInfo.getSeatIds());
