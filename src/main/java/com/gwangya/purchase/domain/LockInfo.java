3a31bbd33be7785115aab901b77ddc8772f6e85d
SOHEELEE408 <dlwlsrl33@gmail.com>
2024-03-19 22:42:40 +0900

    refactor : 생성자 변경

--- src/main/java/com/gwangya/purchase/domain/LockInfo.java
+++ src/main/java/com/gwangya/purchase/domain/LockInfo.java
@@ -1,9 +1,9 @@
 package com.gwangya.purchase.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public class LockInfo {
 
